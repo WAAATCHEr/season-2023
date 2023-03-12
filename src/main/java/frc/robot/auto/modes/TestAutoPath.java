ca22f6aa41a1add3b17b253e8908cebac1a6bda4
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2023-03-12 21:17:43 +0000

    fuck u

--- src/main/java/frc/robot/auto/modes/TestAutoPath.java
+++ src/main/java/frc/robot/auto/modes/TestAutoPath.java
@@ -30,7 +30,7 @@ public class TestAutoPath extends SequentialCommandGroup{
             //             .alongWith(new RunCommand(() -> elevatorArm.moveElevator(-0.7))
             //                         .until(() -> elevatorArm.getBottomSwitch())),
             // elevatorArm.movePivotCommand(() -> ElevatorArm.PivotPosition.DEFAULT),
-            // new WaitCommand(11),
+            // new WaitCommand(11),jhh
             swerve.followTrajectoryCommand(path, eventMap, true)
             , swerve.chargingStationCommand()
         );
