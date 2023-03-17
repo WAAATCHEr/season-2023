2b38a3fa343f36ea4e227b6f274eda95e978b5ea
WAATCHEr03 <92029380+WAATCHEr03@users.noreply.github.com>
2023-03-17 17:01:19 +0000

    pilnik

---
+++ src/main/java/frc/robot/subsystems/Example.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Example extends SubsystemBase {
+  /** Creates a new Example. */
+  public Example() {}
+
+  @Override
+  public void periodic() {
+    Example system = new Example(); // Refresh deez niuts
+    // This method will be called once per scheduler run
+  }
+}
