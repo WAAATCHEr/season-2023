595371dce9b8d6823a4a48392854ab625cf61556
Edward <55427391+UnknownPaws@users.noreply.github.com>
2023-03-17 13:39:54 +0000

    Friction pad L

--- src/main/java/frc/robot/layout/DriverMap.java
+++ src/main/java/frc/robot/layout/DriverMap.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Config;
-import frc.robot.subsystems.FrictionPad;
 import frc.robot.subsystems.Swerve;
 import frc.robot.util.controllers.CommandMap;
 import frc.robot.util.controllers.GameController;
@@ -26,21 +25,12 @@ public abstract class DriverMap extends CommandMap {
     swerve.setDefaultCommand(swerve.driveCommand(this::getChassisSpeeds));
   }
 
-  void registerFrictionPad() {
-    var frictionPad = FrictionPad.getInstance();
-    getFrictionPadDeployButton().onTrue(new InstantCommand(() -> frictionPad.deploy()));
-    getFrictionPadRetractButton().onTrue(new InstantCommand(() -> frictionPad.retract()));
-  }
-
   @Override
   public void registerCommands() {
 
     if (Config.Subsystems.SWERVE_ENABLED) {
       registerSwerve();
     }
-
-    if (Config.Subsystems.FRICTION_PAD_ENABLED) {
-      registerFrictionPad();
-    }
+    
   }
 }
