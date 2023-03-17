50cfa3836e97896b9481da7dfed6048b99414943
Edward <55427391+UnknownPaws@users.noreply.github.com>
2023-03-17 15:41:19 +0000

    Enable and disable controllers from config

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -10,6 +10,12 @@ public class Config {
     public static final boolean ELEVATOR_ARM_ENABLED = true;
   }
 
+  public class Controllers {
+    public static final boolean DRIVER_ENABLED = true;
+    public static final boolean OPERATOR_ENABLED = true;
+    public static final boolean TESTER_ENABLED = false;
+  }
+
   public static ButtonMap getDriverController() {
     return new Xbox();
   }
