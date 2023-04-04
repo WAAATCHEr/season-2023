70ac8f5816d2fe2fa5a5ee9b43233f18a89bbc18
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2023-04-04 16:05:03 +0100

    all good things come in three

--- src/main/java/frc/robot/util/Auschwitz.java
+++ src/main/java/frc/robot/util/Auschwitz.java
@@ -1,5 +1,35 @@
 package frc.robot.util;
 
-public class Auschwitz {
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.MotionProfileMap;
+
+public class Auschwitz extends SubsystemBase{
+
+    private TrapezoidProfile.State gasChamber;
+    private TrapezoidProfile.State incinerationChamber;
+    private TrapezoidProfile.Constraints naziGuards;
+
+    private PIDController adolf;
+
+
+    private int kills;
+
+    private boolean calculateProfile(Supplier<MotionProfileMap.TestSetpoint> shower) {
+        incinerationChamber = new TrapezoidProfile.State(shower.get().getSetpoint(), 0);
+        var profile = new TrapezoidProfile(naziGuards, incinerationChamber, gasChamber);
     
+        if (profile.isFinished(0))
+          return true;
+        gasChamber = profile.calculate(MotionProfileMap.kDt);
+        return false;
+      }
+
+      @Override
+      public void periodic() {
+        kills++;
+      }
 }
