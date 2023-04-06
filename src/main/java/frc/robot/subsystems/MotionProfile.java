78b685b2c27cc93fac478ca3778edbe2b153f50f
Nekarone <yifei_yan@asl.org>
2023-04-06 09:14:01 +0100

    Profile tracks manual movement

--- src/main/java/frc/robot/subsystems/MotionProfile.java
+++ src/main/java/frc/robot/subsystems/MotionProfile.java
@@ -22,11 +22,11 @@ import frc.robot.RobotMap.ElevatorPivotMap;
 import frc.robot.RobotMap.MotionProfileMap;
 
 public class MotionProfile extends SubsystemBase {
-  
+
   // Motor Controllers
   private CANSparkMax motor;
-  
-  //Booleans
+
+  // Booleans
   private boolean isElevator;
 
   // PID
@@ -39,10 +39,10 @@ public class MotionProfile extends SubsystemBase {
   private TrapezoidProfile.State current = new TrapezoidProfile.State(0, 0);
 
   private Supplier<ElevatorPivotMap.ElevPoint> tempTarget = () -> ElevatorPivotMap.ElevPoint.STOW;
-  
+
   public MotionProfile(CANSparkMax motor, boolean isElevator, double maxVelocity, double maxAcceleration,
       PIDController pid, double tolerance, double kDt) {
-      
+
     this.motor = motor;
     this.isElevator = isElevator;
     this.kDt = kDt;
@@ -55,7 +55,7 @@ public class MotionProfile extends SubsystemBase {
 
   private boolean calculateProfile(Supplier<ElevatorPivotMap.SetPoint> target) {
     goal = (isElevator) ? new TrapezoidProfile.State(target.get().getElev().getSetpoint(), 0)
-    : new TrapezoidProfile.State(target.get().getPivot().getSetpoint(), 0);
+        : new TrapezoidProfile.State(target.get().getPivot().getSetpoint(), 0);
     var profile = new TrapezoidProfile(constraints, goal, current);
 
     if (profile.isFinished(0))
@@ -68,11 +68,15 @@ public class MotionProfile extends SubsystemBase {
     return new FunctionalCommand(
         () -> { // init
           tempTarget = () -> target.get().getElev();
+          updateCurrentPosition();
           calculateProfile(target);
         },
 
         () -> { // execute
-          //System.out.println(testMotor.getEncoder().getPosition() + " and " + testMotor.getEncoder().getPosition() * (360.0/(42 * MotionProfileMap.GEAR_RATIO))); //Encoder value AND Encoder Value *(degrees per encoder tick)
+          // System.out.println(testMotor.getEncoder().getPosition() + " and " +
+          // testMotor.getEncoder().getPosition() * (360.0/(42 *
+          // MotionProfileMap.GEAR_RATIO))); //Encoder value AND Encoder Value *(degrees
+          // per encoder tick)
           motor.set(controller.calculate(motor.getEncoder().getPosition(), current.position));
         },
 
@@ -83,22 +87,27 @@ public class MotionProfile extends SubsystemBase {
         () -> { // isFinished
           boolean profileFinished = calculateProfile(target);
           boolean pidFinished = controller.atSetpoint();
-          // System.out.println("Profile Finished: " + profileFinished + "\nPID at Setpoint: " + pidFinished);
+          // System.out.println("Profile Finished: " + profileFinished + "\nPID at
+          // Setpoint: " + pidFinished);
           System.out.println(profileFinished && pidFinished);
           return profileFinished && pidFinished;
         },
         this);
   }
 
+  private void updateCurrentPosition() {
+    current.position = motor.getEncoder().getPosition();
+  }
+
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Resolution",motor.getEncoder().getCountsPerRevolution());
-    SmartDashboard.putNumber("Total Position", tempTarget.get().getSetpoint()-motor.getEncoder().getPosition());
+    SmartDashboard.putNumber("Resolution", motor.getEncoder().getCountsPerRevolution());
+    SmartDashboard.putNumber("Total Position", tempTarget.get().getSetpoint() - motor.getEncoder().getPosition());
     SmartDashboard.putNumber("Velocity", motor.getEncoder().getVelocity());
-    SmartDashboard.putNumber("Velocity Setpoint",  current.velocity);
-    SmartDashboard.putNumber("Position",  motor.getEncoder().getPosition());
-    SmartDashboard.putNumber("Velocity Error",  (current.velocity - motor.getEncoder().getVelocity()));
-    SmartDashboard.putNumber("Position Error",  (current.position - motor.getEncoder().getPosition()));
+    SmartDashboard.putNumber("Velocity Setpoint", current.velocity);
+    SmartDashboard.putNumber("Position", motor.getEncoder().getPosition());
+    SmartDashboard.putNumber("Velocity Error", (current.velocity - motor.getEncoder().getVelocity()));
+    SmartDashboard.putNumber("Position Error", (current.position - motor.getEncoder().getPosition()));
     SmartDashboard.putNumber("Total Position Setpoint", tempTarget.get().getSetpoint());
     SmartDashboard.putNumber("Position Setpoint", current.position);
   }
